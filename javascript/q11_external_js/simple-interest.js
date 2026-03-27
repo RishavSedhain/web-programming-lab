c84dcb01c6865faa39b340cd3a79857d179b5e10
RishavSedhain <rishavsedhain@gmail.com>
2026-03-27 21:32:55 +0530

    Add CSS, HTML, and JavaScript assignment files

---
+++ javascript/q11_external_js/simple-interest.js
@@ -0,0 +1,14 @@
+﻿function findSimpleInterest(principal, rate, time) {
+    return (principal * rate * time) / 100;
+}
+
+function calculateSI() {
+    var principal = parseFloat(document.getElementById("principal").value);
+    var rate = parseFloat(document.getElementById("rate").value);
+    var time = parseFloat(document.getElementById("time").value);
+
+    var simpleInterest = findSimpleInterest(principal, rate, time);
+
+    document.getElementById("result").innerHTML =
+        "Simple Interest: " + simpleInterest.toFixed(2);
+}
